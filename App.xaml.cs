1e0c88cfaf189ab7d7ed88cfb2f16519d502b04f
Henrik Wiener <henrik.wiener@outlook.com>
2022-12-09 23:29:17 -0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace ConcurrencyUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		InitializeComponent();
+		var width = 1920;
+		var height = 1200;
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+#if WINDOWS
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+			Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			//TODO: this is hardcoded stuff for fullhd
+			appWindow.MoveAndResize(new Windows.Graphics.RectInt32((1920 / 2) - width / 2, (1200 / 2) - height / 2, width, height));
+#endif
+		});
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace ConcurrencyUI;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+	protected override void OnAppearing()
+	{
+		TitleLabel.ScaleTo(3, 1000);
+	}
+
+	private async void OnSumBtnClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			double conc = Convert.ToDouble(ConcurrentEntry.Text);
+			double synch = Convert.ToDouble(SynchronousEntry.Text);
+
+			await RunSynchRunAsynch(conc, synch);
+		}
+		catch (FormatException ex)
+		{
+			await DisplayAlert("Invalid entry", "Please enter a valid number to sum to", "Ok");
+		}
+	}
+
+	public async Task RunSynchRunAsynch(double conc, double synch)
+	{
+		var url = "http://localhost:8000/api/concurrent"; // access the login endpoint to receive access token from authorization server
+		using var client = new HttpClient();
+
+		client.DefaultRequestHeaders.Add("concurrency", $"{conc}"); // add number to sum to 
+
+		var response = await client.GetAsync(url);
+		var result = await response.Content.ReadAsStringAsync();
+		TimeSpan concurrentTimeElapsed = TimeSpan.Parse(JObject.Parse(result)["concurrencytime"].ToString());
+		double concurrentSum = Convert.ToDouble(JObject.Parse(result)["concurrencysum"].ToString());
+		ConcurrentEntry.Text = concurrentSum.ToString();
+		ConcurrentTime.Text = concurrentTimeElapsed.ToString();
+
+		client.DefaultRequestHeaders.Remove("concurrency");
+		client.DefaultRequestHeaders.Add("synchronous", $"{synch}"); // add number to sum to 
+
+
+		var newUrl = "http://localhost:8000/api/synchronous"; // get request to user endpoint to receive protected user information and log user in
+
+		var response2 = await client.GetAsync(newUrl);
+
+		var result2 = await response2.Content.ReadAsStringAsync();
+		TimeSpan synchtTimeElapsed = TimeSpan.Parse(JObject.Parse(result2)["synchtime"].ToString());
+		double synchSum = Convert.ToDouble(JObject.Parse(result2)["synchsum"].ToString());
+
+		SynchronousEntry.Text = synchSum.ToString();
+		SynchronousTime.Text = synchtTimeElapsed.ToString();
+
+	} // End method
+}
+
